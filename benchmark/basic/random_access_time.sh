f727296487cbc01075201f87e3ed3d7146f75778
Shaik_Mastan <shaikmastadino@gmail.com>
2022-02-14 11:05:37 +0530

    Minor change

--- benchmark/basic/random_access_time.sh
+++ benchmark/basic/random_access_time.sh
@@ -16,11 +16,11 @@ do
     echo ""
     # Compressing
     g++ -Wall -w -I/usr/local/include/ -c dictionary_compression_chunks.cpp -lm
-    g++ -L/usr/local/lib/ dictionary_compression_chunks.o -o dict_compressor_chunks -lzstd
+    g++ -L/usr/local/lib/ dictionary_compression_chunks.o -o dict_compressor_chunks -lzstd -lsdsl
     ./dict_compressor_chunks $filePath $dictPath $chunkSize
 
     # # Decompressing
     g++ -Wall -w -I/usr/local/include/ -c dictionary_decompression_chunks.cpp -lm
-    g++ -L/usr/local/lib/ dictionary_decompression_chunks.o -o dict_decompressor_chunks -lzstd
+    g++ -L/usr/local/lib/ dictionary_decompression_chunks.o -o dict_decompressor_chunks -lzstd -lsdsl
     ./dict_decompressor_chunks "$filePath"_dict_chunks.zst $dictPath
 done
