60be0ff7a49d473e35c14c3a16578474c42c6538
Shaik_Mastan <shaikmastadino@gmail.com>
2022-04-05 09:49:04 +0530

    Print compression size in compare.sh

--- benchmark/compare.sh
+++ benchmark/compare.sh
@@ -1,8 +1,9 @@
 FILE_NAME="$1"
 
 # Compression
-noi=5
+noi=1
 avgCompressionTime=0
+compressedFileSize=0
 avgDecompressionTime=0
 for (( i=0; i<$noi; i+=1))
 do
@@ -10,6 +11,7 @@ do
     gzip -f "$FILE_NAME"
     end_time=$(date +%s.%3N)
     avgCompressionTime=$(echo "scale=3; $avgCompressionTime + $end_time - $start_time" | bc)
+    compressedFileSize=$(stat -c%s "$FILE_NAME".gz)
     # avgCompressionTime=$(( $avgCompressiontime + $elapsed ))
 
     start_time=$(date +%s.%3N)
@@ -21,5 +23,6 @@ done
 
 avgCompressionTime=$(echo "scale=3; $avgCompressionTime / $noi" | bc)
 avgDecompressionTime=$(echo "scale=3; $avgDecompressionTime / $noi" | bc)
+echo "Compressed file size: $(echo "scale=3; $compressedFileSize/(2^30)" | bc) GB"
 echo "Compression time: $avgCompressionTime ms"
 echo "Decompression time: $avgDecompressionTime ms"
